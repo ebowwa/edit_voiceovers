6c679e5366232359cd86de8f6b3d27fa02ffdba4
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-01-30 15:11:55 -0800

    Update create_voice.py

--- tts/cloning/create_voice.py
+++ tts/cloning/create_voice.py
@@ -1,105 +1,46 @@
-# resemble-clone-voice-recording/main.py
+import base64
 
-# -- snipped --
+def read_consent_file(filepath):
+    """
+    Reads a file and returns its base64 encoded contents.
+    """
+    try:
+        with open(filepath, 'rb') as file:
+            file_contents = file.read()
+            return base64.b64encode(file_contents).decode('utf-8')
+    except IOError as e:
+        print(f"Error reading file {filepath}: {e}")
+        return None
 
-def create_voice(voice_name):
+def create_voice(voice_name, consent_filepath):
+    """
+    Submits a request to create a voice using the Resemble API.
+    """
     print(f"Submitting request to Resemble to create a voice: {voice_name}")
 
-    # Make request to the API, note that we do not provide a callback_uri so this
-    # will request will execute synchronously.
-    #
-    # This will trigger the voice creation process but not the voice building process
-    # we need to trigger that through the voice building API
-    #
-    # https://docs.app.resemble.ai/docs/resource_voice/build/
-    #
-
-    base64_consent = ''
-
-    # In order to clone a voice, you MUST provide a base64 encoded consent file
-    #
-    # https://docs.app.resemble.ai/docs/resource_voice/create#voice-consent
-    #
-    # FIXME: You will need update this function to the path to your consent file
-
-    with open('FIXME: path/to/consent/file', 'rb') as file:
-        file_contents = file.read()
-
-        # Encode the file contents as Base64
-        base64_consent = base64.b64encode(file_contents).decode('utf-8')
+    base64_consent = read_consent_file(consent_filepath)
+    if base64_consent is None:
+        print("Failed to read consent file.")
+        return None
 
     response = Resemble.v2.voices.create(name=voice_name, consent=base64_consent)
+    return handle_api_response(response, voice_name)
 
-    voice = response['item']
-
-    if response['success']:
+def handle_api_response(response, voice_name):
+    """
+    Handles the response from the API.
+    """
+    if response.get('success'):
         voice = response['item']
         voice_status = voice['status']
         voice_uuid = voice['uuid']
-
         print(f"Response was successful! {voice_name} has been created with UUID {voice_uuid}. The voice is currently {voice_status}.")
-
         return voice_uuid
     else:
         print("Response was unsuccessful!")
-
-        # In case of an error, print the error to STDOUT
-        print(response)
-
-        return None
-
-# -- snipped --
-
-import base64
-from resemble import Resemble
-
-def create_voice(voice_name, consent_file_path, dataset_url=None, callback_uri=None):
-     # Make request to the API, note that we do not provide a callback_uri so this
-    # will request will execute synchronously.
-    #
-    # This will trigger the voice creation process but not the voice building process
-    # we need to trigger that through the voice building API
-    #
-    # https://docs.app.resemble.ai/docs/resource_voice/build/
-    #
-    """
-
-    Args:
-    voice_name (str): Name of the voice to be created.
-    consent_file_path (str): Path to the consent audio file.
-    dataset_url (str, optional): URL to a dataset for training the voice.
-    callback_uri (str, optional): Callback URI for notification upon voice training completion.
-
-    Returns:
-    str: UUID of the created voice, or None if creation failed.
-    """
-    # Read the consent file and encode it in base64
-    with open(consent_file_path, 'rb') as file:
-        file_contents = file.read()
-        base64_consent = base64.b64encode(file_contents).decode('utf-8')
-
-    # In order to clone a voice, you MUST provide a base64 encoded consent file
-    #
-    # https://docs.app.resemble.ai/docs/resource_voice/create#voice-consent
-    #
-    # FIXME: You will need update this function to the path to your consent file
-
-    # Create the voice using the Resemble AI API
-    params = {
-        "name": voice_name,
-        "consent": base64_consent,
-        "dataset_url": dataset_url,
-        "callback_uri": callback_uri
-    }
-    response = Resemble.v2.voices.create(**params)
-
-    # Handle the response
-    if response.get('success'):
-        voice = response['item']
-        print(f\"Voice created successfully. UUID: {voice['uuid']}\")
-        return voice['uuid']
-    else:
-        print(\"Failed to create voice.\")
         print(response)
         return None
+
+# Usage example
+# voice_uuid = create_voice("ExampleVoice", "path/to/consent/file")
 
