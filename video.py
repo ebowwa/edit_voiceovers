596bbd45270f4a0e392cd23f7d8fd1d5d8e06e3a
ebowwa <ec.arbee1@gmail.com>
2024-02-01 01:43:02 -0800

    frame rates + collages fixed

--- video.py
+++ video.py
@@ -3,9 +3,9 @@ from video.collage_build import main as collage_main
 def higher_level_function():
     video_file_path = 'public/AdobeStock_607123108_Video_HD_Preview.mov'
     target_frame_rate = 27
-    max_frames_per_collage = 6
+     
     
-    collage_directory = collage_main(video_file_path, target_frame_rate, max_frames_per_collage)
+    collage_directory = collage_main(video_file_path, target_frame_rate)
     print(f"Collages are saved in: {collage_directory}")
     # Implement any additional logic you need after generating the collages
 
