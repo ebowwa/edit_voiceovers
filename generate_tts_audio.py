ba58e56c0ad6ddb297dc8c421208fe89f6d9ce7b
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-02-02 05:51:04 +0000

    update

--- generate_tts_audio.py
+++
@@ -1,9 +0,0 @@
-# works with the resemble api 
-# future support for other tts providers and cloud hosting options
-from tts.text_to_speech import generate_speech
-
-def generate_response_audio(response_text, sequence_number):
-    project_uuid = "0448305f"
-    voice_uuid = "d3e61caf"
-    title = "Generated Content Title"
-    generate_speech(response_text, project_uuid, voice_uuid, title, sequence_number)
---
+++ prompts/narrations/concise-notes.md
@@ -0,0 +1,2 @@
+you make notes about the frame rates you are sequential shown as if you are watching the video, you mostly are, make excellent notes so that another llm can write an overlaying story
+- be concise
